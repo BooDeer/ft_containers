8e6a44dfcfd231b157c005a2e2f6aeed893c374e
BooDeer <hsissi3a@gmail.com>
2022-02-11 10:11:13 +0100

    Working on the vector iterator

--- iterator.hpp
+++ iterator.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
 #include <iostream>
+
+
+template <class _Iter>
+class	_ft_random_iterator: public std::iterator<std::random_access_iterator_tag, _Iter>
+{
+	private:
+		_Iter*	_ptr;
+	public:
+		typedef _Iter					iterator_type;
+		//TODO: requires iterator_traits class to be implemented
+		typedef typename iterator_traits<iterator_type>::iterator_category	iterator_category;
+};
