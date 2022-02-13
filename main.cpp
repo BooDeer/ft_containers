ab61d0ba93c5c65b9acb1a1f94a6847c064fb68a
BooDeer <hsissi3a@gmail.com>
2022-02-13 15:25:55 +0100

    working on the vector

--- main.cpp
+++ main.cpp
@@ -6,29 +6,80 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 14:54:59 by hboudhir          #+#    #+#             */
-/*   Updated: 2022/02/10 11:47:36 by hboudhir         ###   ########.fr       */
+/*   Updated: 2022/02/12 19:06:15 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "stack.hpp"
 #include "macro.hpp"
-#include <stack>
+// #include "stack.hpp"
+// #include <stack>
 #include <vector>
+// #include <map>
+#include "vector.hpp"
 
+/*
+	// template <class iter>
+	// size_t do_distance(iter it,iter end, std::random_access_iterator_tag)
+	// {
+	// 	return end - it;
+	// }
+
+	// template <class iter>
+	// size_t do_distance(iter it,iter end, std::input_iterator_tag)
+	// {
+	// 	size_t k = 0;
+	// 	while(it != end)
+	// 		k++, it++;
+	// 	return k;
+	// }
+
+	// template <class iter>
+	// size_t ft_distance(iter it, iter end)
+	// {
+	// 	do_distance(it,end, iterator_triats<iter>::iterator_category())
+	// }
+
+	// template <class T>
+	// void	print(void)
+	// {
+	// 	T* c;
+	// 	std::cout << "test" << std::endl;
+	// }
+
+	// template<>
+	// void	print<int>( void )
+	// {
+	// 	std::cout << "test22" << std::endl;
+	// }
+*/	
 
 int main( void )
 {
+	
+	LOG("\n\n=============================== STD::MAP  ================================\n\n");
+	ft::vector<std::string>	test(5, "tet");
+	
+	LOG(test.max_size());
+
+	
 	LOG("============================== STD::VECTOR  ==============================\n\n");
-	std::vector<int> testV;
-	
-	LOG("=============================== FT::STACK  ===============================\n\n");
-	std::stack<int> test1;
+	std::vector<int> testV(3, 5);
 
-	LOG("=============================== STD::STACK ===============================\n\n");
-	
-	ft::stack<int> test2;
+	testV.assign(1, 1);
+	LOG(!testV.empty());
 
-	
+
+
+
+
+
+
+
+
+
+
+
+
 	return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -3,81 +3,158 @@
 #include <iostream>
 #include <memory>
 
-template < class T, class Alloc = std::allocator<T> > class Vector
-{
-	//! Member types.
-	// Public--<begin>
-	public:
-	typedef T												value_type; 		//* First parametre of the templated class. (T)
-	typedef Alloc											allocator_type; 	//* Second parametre of the templated class. (Alloc)
-	typedef	typename allocator_type::reference				reference; 			//* Reference to the allocator_type. (AKA the second parametre of the templated class)
-	typedef	typename allocator_type::const_reference		const_reference;	//* Const reference to the allocator_type. (AKA the second parametre of the templated class)
-	typedef	typename allocator_type::pointer				pointer;
-	typedef	typename allocator_type::const_pointer			const_pointer;
-	//TODO: implement the iterators to be used.
-	typedef	iterator<pointer>								iterator;
-	typedef	iterator<const_pointer>							const_iterator;
-	typedef										reverse_iterator;
-	typedef	xxx									const_reverse_iterator;
-	typedef	xxx									difference_type;
-	//TODO <=======================================>
-	typedef	size_t								size_type;
-	// Public--<end>
-	//!=========================================
+namespace ft{
 
-	//! Orthodox canonical form (Member functions)
-	public:
-		Vector( void );
-		Vector(const Vector& src);
-		Vector& operator=(const Vector& rhs);
-		~Vector( void );
-	//!=========================================
+	template < class T, class Alloc = std::allocator<T> > class vector
+	{
+		//! Member types.
+		// Public--<begin>
+		public:
+			//! Typedefs only create a new alias for an existing type, you need to create a variable to hold that type before using it
 
-	//! Iterator member functions.
-	public:
-		//TODO
-	//!=========================================
+			typedef T												value_type; 		//* First parametre of the templated class. (T)
+			typedef Alloc											allocator_type; 	//* Second parametre of the templated class. (Alloc used to allocate memory)
+			typedef	typename allocator_type::reference				reference; 			//* Reference to the allocator_type. (Equal to T&)
+			typedef	typename allocator_type::const_reference		const_reference;	//* Const reference to the allocator_type. (Equal to const T&)
+			typedef	typename allocator_type::pointer				pointer;			//* Pointer to the allocated object. (Equal to T*)
+			typedef	typename allocator_type::const_pointer			const_pointer;		//* Const pointer to the allocated object. (Equal to const T*)
+			// //TODO: implement the iterators to be used.
+			// typedef	iterator<pointer>								iterator;
+			// typedef	iterator<const_pointer>							const_iterator;
+			// typedef													reverse_iterator;
+			// typedef	xxx												const_reverse_iterator;
+			// typedef	xxx												difference_type;
+			// //TODO <=======================================>
+			typedef	size_t											size_type;
+		// Public--<end>
+		//!=========================================
+		//! Orthodox canonical form (Member functions)
+		public:
+		  	//* The default constructor.
+			explicit vector( const allocator_type& alloc = allocator_type() ): __Vec(nullptr),
+																	__Size(0), __Capacity(0), __Alloc(alloc)
+			{
+				//TODO: set the values to default ( 0/nullptr )
+			};
 
-	//! Capacity member functions.
-	public:
-		size_type	size() const;
-		size_type	max_size() const;
-		void		resize(size_type n, value_type val = value_type());
-		void		reserve(size_type n);
-		bool		empty() const;
-		size_type	capacity() const;
-	//!=========================================
+			//* The fill constructor.
+			explicit vector(size_type n, const value_type& val = value_type()
+					, const allocator_type& alloc = allocator_type() ): __Vec(nullptr),
+																	__Size(0), __Capacity(n), __Alloc(alloc)
+			{
+				__Vec = __Alloc.allocate(n);
+				for (int i = 0; i < n; i++)
+					__Alloc.construct(__Vec + i, val);
+				LOG(__Vec[3]);
+			};
 
-	//! Element access member functions.
-	public:
-		reference			back();
-		const_reference		back() const; //TODO: Search what's the difference between this notation and the one above it.
-		reference			front();
-		const_reference		front() const;
-		reference			at(size_type n);
-		const_reference		at(size_type n) const;
-		reference			operator[](size_type n);
-		const_reference		operator[](size_type n) const;
-	//!=========================================
+			//* The range constructor.
+			//TODO: implement the range constructor.
+			// template <class InputIterator>
+        	// vector (InputIterator first, InputIterator last,
+            //     	const allocator_type& alloc = allocator_type());
 
-	//! Modifiers member functions.
-	public:
-		void			clear();
-		void			swap(Vector& x);
-		iterator		erase(iterator position);
-		iterator		erase(iterator first, iterator last);
-		iterator		insert(iterator position, const value_type& val);
-		void			insert(iterator position, size_type n, const value_type& val);
-		template<class InputIterator>
-		void			insert(iterator position, InputIterator first, InputIterator last);
-		void			pop_back();
-		void			push_back(const value_type& val);
-		template<class InputIterator>
-		void			assign(InputIterator first, InputIterator last);
-		void			assign(size_type n, const value_type &val);
-	//!=========================================
+			//* The copy constructor.
+			// vector(const vector& x)
+			// {
+			// 	//TODO: implement the assignement operator.
+			// 	*this = x;
+			// }
 
-	//! Allocator member function
-	public:
-		allocator_type get_allocator() const;
-};
+			// vector(const vector& src);
+			// vector& operator=(const vector& rhs);
+			~vector( void )
+			{
+				//TODO: implement the fucking destructor.
+
+			};
+		//!=========================================
+
+		//! Iterator member functions.
+		public:
+			//TODO
+		//!=========================================
+
+		//! Capacity member functions.
+		public:
+			size_type	size() const //* Returns __Size. (How many elements currently in the vector)
+			{
+				return	this->__Size;
+			};
+			size_type	max_size() const //* Returns the maximum allocation possible. (same value as allocate.max_size())
+			{
+				return __Alloc.max_size();
+			};
+			// void		resize(size_type n, value_type val = value_type());
+			// void		reserve(size_type n);
+			bool		empty() const
+			{
+				return __Size ? false : true; //! Still not tested.
+			};
+			// size_type	capacity() const;
+		//!=========================================
+
+		//! Element access member functions.
+		// public:
+		// 	reference			back();
+		// 	const_reference		back() const; //TODO: Search what's the difference between this notation and the one above it.
+		// 	reference			front();
+		// 	const_reference		front() const;
+		// 	reference			at(size_type n);
+		// 	const_reference		at(size_type n) const;
+		// 	reference			operator[](size_type n);
+		// 	const_reference		operator[](size_type n) const;
+		//!=========================================
+
+		//! Modifiers member functions.
+		// public:
+			void			clear()
+			{
+				for (int i = 0; i < __Size; i++)
+				{
+					__Alloc.destroy(__Vec[i]);
+				}
+				__Size = 0;
+				//TODO: Decide whether I reallocate memory for the container or not. (IMO it's better to reallocate to not waste space)
+				//TODO: https://www.cplusplus.com/reference/vector/vector/assign/ "This causes an automatic reallocation of the allocated storage space if -and only if- the new vector size surpasses the current vector capacity."
+			};
+		// 	void			swap(Vector& x);
+		// 	iterator		erase(iterator position);
+		// 	iterator		erase(iterator first, iterator last);
+		// 	iterator		insert(iterator position, const value_type& val);
+		// 	void			insert(iterator position, size_type n, const value_type& val);
+		// 	template<class InputIterator>
+		// 	void			insert(iterator position, InputIterator first, InputIterator last);
+		// 	void			pop_back();
+		// 	void			push_back(const value_type& val);
+		// 	template<class InputIterator>
+		// 	void			assign(InputIterator first, InputIterator last);
+			void			assign(size_type n, const value_type &val)
+			{
+				this->clear();
+				//TODO: check if there's enough storage space, otherwise allocate new memory.
+				if (n >= __Capacity)
+				{
+					//TODO:
+				}
+
+				//
+				for (int i = 0; i < n; i++)
+				{
+					__Alloc.construct(__Vec[i], val);
+				}
+			};
+		//!=========================================
+
+		//! Allocator member function
+		// public:
+		// 	allocator_type get_allocator() const;
+
+
+		private:
+			pointer			__Vec;				//* The array of elements.
+			size_t			__Size;				//* The current size of the vector, meaning how many elements currently holding.
+			size_t			__Capacity;			//* The maximum size allocated for the vector.
+			allocator_type	__Alloc;				//* The allocator to be used
+	};
+}
