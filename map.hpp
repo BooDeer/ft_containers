fb37ca7562d4aa3e9441c3d5db7d50564b79bcf3
BooDeer <hsissi3a@gmail.com>
2022-02-23 15:47:34 +0100

    working on the map container

---
+++ map.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+namespace ft
+{
+
+	template<class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key, T> >
+	> class map
+	{
+		
+	};
+};
