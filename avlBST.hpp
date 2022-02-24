fd4dd093e4c4f4e28600fa72d0806c7d01199c28
BooDeer <hsissi3a@gmail.com>
2022-02-24 18:10:02 +0100

    Implementing the binary tree

--- avlBST.hpp
+++ avlBST.hpp
@@ -2,19 +2,73 @@
 
 #include <iostream>
 
-template <class __T> // int 1 2 3 4
+template <typename T>
+typedef struct Node
+{
+	struct Node		*parent;		//* The parent node.
+	struct Node		*rchild;		//* The right child node.
+	struct Node		*lchild;		//* The left child node.
+	T				*data;			//* Pointer to the data hold by the node.
+
+	Node (Node *__pr, Node *__rch, Node *__lch, T* __data): parent(__pr), rchild(__rch), lchild(__lch), data(__data);
+}	node;
+
+
+template <class __T, class __Compare = std::less<__T>, class __Allocator = std::allocator<__T> >
 class AvlBST
 {
 	public:
-		AvlBST( void );
-		AvlBST( const AvlBST& src );
-		AvlBST& operator=( const AvlBST& rhs);
-		~AvlBST( void );
-	protected:
+		//* Types.
+		typedef __T									value_type;
+	
+	public:
+		//* Copy contructor.
+		AvlBST( const AvlBST& src )
+		{
+			*this = src;
+		};
+		//* Assignement operator.
+		AvlBST& operator=( const AvlBST& rhs)
+		{
+			__PAR	= rhs.__PAR;
+			__RC	= rhs.__RC;
+			__LC	= rhs.__LC;
+			__Value	= rhs.__Value;
+			return *this;
+		};
+		//* Destructor
+		~AvlBST( void )
+		{
+			//TODO: to be implemented.
+			
+		};
+	//! Insertion methods.
+	public:
+		AvlBST( void ): __PAR(nullptr), __RC(nullptr), __LC(nullptr), __Value(__T()) {};
+		void	insert(value_type __src)
+		{
+			if ()
+		};
 
+
+
+
+	//! ======================================================================================
 	private:
-		AvlBST*	__PAR;					//* The parent node.
-		AvlBST*	__RC;					//* The right child node.
-		AvlBST* __LC;					//* The left child node.
-		__T		__Value;
+		//? Should I keep the default constructor private or not?
+		//* Private attributes.
+		AvlBST*								__PAR;					//* The parent node.
+		AvlBST*								__RC;					//* The right child node.
+		AvlBST* 							__LC;					//* The left child node.
+		__T									__Value;				//* The hold data.
+		node								__root;
+		__Compare							__cmp;
 };
+
+/* 
+
+const char * hamada
+char const * hamada
+char * const ha
+
+*/
