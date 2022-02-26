cc0b8f925d3f8ab8ebeb1067890465f208eeb4e3
BooDeer <hsissi3a@gmail.com>
2022-02-26 17:12:04 +0100

    mapIterator uncomplete

--- mapIterator.hpp
+++ mapIterator.hpp
@@ -4,65 +4,65 @@
 #include "avlBST.hpp"
 
 
-template <class __Iter>
-class	mapIterator
-{
-	public:
-		//* Types.
-		typedef __Iter													iterator_type;
-		typedef std::bidirectional_iterator_tag							iterator_category;
-		typedef	iterator_type											value_type;
-		typedef	std::ptrdiff_t											difference_type;
-		typedef	__Iter*													pointer;
-		typedef	__Iter&													reference;
+// template <class __Iter, class Compare = std::less<__Iter> >
+// class	mapIterator
+// {
+// 	public:
+// 		//* Types.
+// 		typedef __Iter													iterator_type;
+// 		typedef std::bidirectional_iterator_tag							iterator_category;
+// 		typedef	iterator_type											value_type;
+// 		typedef	std::ptrdiff_t											difference_type;
+// 		typedef	__Iter*													pointer;
+// 		typedef	__Iter&													reference;
 
 
 
-	//! The canonical form.
-	public:
-		//* Default constructor.
-		mapIterator ( void ) {
-			LOG("[mapIterator] Default constructor.");
-		};
-		// // Constructor with a parameter.
-		//* Copy constructor.
-		mapIterator(const mapIterator& src) {
-			*this = src;
-		}
+// 	//! The canonical form.
+// 	public:
+// 		//* Default constructor.
+// 		mapIterator ( void ) {
+// 			LOG("[mapIterator] Default constructor.");
+// 		};
+// 		// // Constructor with a parameter.
+// 		//* Copy constructor.
+// 		mapIterator(const mapIterator& src) {
+// 			*this = src;
+// 		}
 
-		mapIterator& operator=( const mapIterator& rhs )
-		{
-			this->__Tree = rhs.__Tree;
+// 		mapIterator& operator=( const mapIterator& rhs )
+// 		{
+// 			this->__Tree = rhs.__Tree;
 			
-			return *this;
-		}
-		~mapIterator ( void )
-		{
-			//TODO: Implement the destructor.
+// 			return *this;
+// 		}
+// 		~mapIterator ( void )
+// 		{
+// 			//TODO: Implement the destructor.
 			
-		}
+// 		}
 
-	//TODO: Insert, delete and other member functions related to the tree shall be converted to private member functions later.
-	//TODO: (currently public just for the sake of testing)
-	public:
-		void	insert(const value_type& node)
-		{
-			this->__Tree.insertNode(node);
-		}
+// 	//TODO: Insert, delete and other member functions related to the tree shall be converted to private member functions later.
+// 	//TODO: (currently public just for the sake of testing)
+// 	public:
+// 		void	insert(const value_type& node)
+// 		{
+// 			this->__Tree.insertNode(node);
+// 		}
 
-		void	deleteNode(const value_type& node)
-		{
-			__Tree.deleteNode(node);
-		}
-	//! Debug functions.
-		void	debug( void )
-		{
-			this->__Tree.printBT(__Tree.__root);
-		}
-
-
+// 		void	deleteNode(const value_type& node)
+// 		{
+// 			__Tree.deleteNode(node);
+// 		}
+// 	//! Debug functions.
+// 		void	debug( void )
+// 		{
+// 			this->__Tree.printBT(__Tree.__root);
+// 		}
 
 
-	private:
-		AvlBST<__Iter>				__Tree;
-};
+
+
+// 	private:
+// 		__Iter				*__Tree;
+// };
