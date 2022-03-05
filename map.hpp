9bea332f47812a84a36ce183d71b864adce42446
BooDeer <hsissi3a@gmail.com>
2022-03-05 13:27:51 +0100

    Added iterator functions + some public methods

--- map.hpp
+++ map.hpp
@@ -4,7 +4,7 @@
 #include "utility.hpp"
 #include "mapIterator.hpp"
 #include "avlBST.hpp"
-
+#include "reverse_iterator.hpp"
 namespace ft
 {
 
@@ -13,23 +13,23 @@ namespace ft
 	{
 		public:
 			//* Types.
-			typedef	Key												key_type;
-			typedef	T												mapped_type;
-			typedef ft::pair<const key_type, mapped_type>			value_type;
-			typedef Compare											key_compare;
-			typedef	Alloc											allocator_type;
-			typedef value_type&										reference;
-			typedef const value_type&								const_reference;
+			typedef	Key														key_type;
+			typedef	T														mapped_type;
+			typedef ft::pair<const key_type, mapped_type>					value_type;
+			typedef Compare													key_compare;
+			typedef	Alloc													allocator_type;
+			typedef typename allocator_type::reference&						reference;
+			typedef typename allocator_type::const_reference&				const_reference;
 
 
-			typedef typename allocator_type::pointer				pointer;
-			typedef typename allocator_type::const_pointer			const_pointer;
+			typedef typename allocator_type::pointer						pointer;
+			typedef typename allocator_type::const_pointer					const_pointer;
 			
 			//* Iterators.
 			typedef	mapIterator<AvlBST<value_type, key_compare> >							iterator;
-			// typedef mapIterator<const AvlBST<value_type, key_compare> >						const_iterator;
-			// typedef reverseMapIterator<iterator>									reverse_iterator;
-			// typedef reverseMapIterator<const_iterator>							const_reverse_iterator;
+			typedef mapIterator<const AvlBST<value_type, key_compare> >						const_iterator;
+			typedef ft::reverse_iterator<iterator>									reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>							const_reverse_iterator;
 			typedef std::ptrdiff_t									difference_type;
 			typedef std::size_t										size_type;
 
@@ -45,12 +45,23 @@ namespace ft
 			// LOG("[map] Default constructor.");
 		};
 		//* Copy constructor.
-		map (const map& src): __Size(src.__Size), __cmp(src.__cmp)
+		map (const map& src): __Size(src.__Size), __cmp(src.__cmp), __TreeRoot(src.__TreeRoot)
 		{
 			//TODO: implement copy constructor.
 
 		}
-
+		//* Range constructor.
+		template <class InputIterator>
+  		map (InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()): __Size(0), __cmp(comp), __Alloc(alloc)
+		{
+			insert(first, last);
+		};
+		map& operator= (const map& x)
+		{
+			__TreeRoot	= x.__TreeRoot;
+			__Size		= x.__Size;
+			return *this;
+		};
 		//! The canonical form =====================================================================
 
 
@@ -71,11 +82,32 @@ namespace ft
 			{
 				return __Alloc.max_size();
 			}
-			void	insert(const value_type& val)
+			ft::pair<iterator, bool>	insert(const value_type& val)
 			{
-				return __TreeRoot.insertNode(val);
+				typename AvlBST<value_type, key_compare, Alloc>::Node*	temp;
+				if ((temp = __TreeRoot.searchNode(__TreeRoot.__root, val)))
+					return ft::make_pair<iterator, bool>(iterator(AvlBST<value_type, key_compare>(__TreeRoot.__root, temp)), false);
+				__TreeRoot.insertNode(val);
+				temp = __TreeRoot.searchNode(__TreeRoot.__root, val);
+				__Size++;
+				return ft::make_pair<iterator, bool>(iterator(AvlBST<value_type, key_compare>(__TreeRoot.__root, temp)), true);
 			};
-		
+			//! Am I retarded or that's how it should be done?
+			//* Yes I am indeed retarded, the hint is to help you reach faster the required node, if not found, then insert from the root.
+			// iterator insert (iterator position, const value_type& val)
+			// {
+			// 	reinterpret_cast<void>(position);
+			// 	return	insert(val).first;
+			// };
+			template <class InputIterator>
+  			void insert (InputIterator first, InputIterator last)
+			{
+				while (first != last)
+				{
+					insert(ft::pair<key_type, mapped_type>((*first).first, (*first).second));
+					first++;
+				}
+			};
 
 		//! Iterator member functions.
 		public:
@@ -83,8 +115,95 @@ namespace ft
 			{
 				return iterator(__TreeRoot.begin());
 			}
+			const_iterator	begin() const
+			{
+				return const_iterator(__TreeRoot.begin());
+			}
 
+			//? Is this working? I don't feel like it should be working but I guess it does work?!
 
+			iterator	end()
+			{
+				return iterator(AvlBST<value_type, key_compare, Alloc>());
+			}
+			const_iterator	end() const
+			{
+				return const_iterator(AvlBST<value_type, key_compare, Alloc>());
+			}
+		//! Operators.
+		void			clear()
+		{
+			while (__Size > 0)
+				this->erase(__TreeRoot.__root->key.first);
+		}
+		mapped_type&	operator[] (const key_type& k)
+		{
+			typename AvlBST<value_type, key_compare, Alloc>::Node*	temp;
+			if ( (temp = __TreeRoot.searchNode(__TreeRoot.__root, ft::make_pair<key_type, mapped_type>(k, mapped_type()))))
+				return temp->key.second;
+			temp = __TreeRoot.insertNode(__TreeRoot.__root,NULL, ft::make_pair<key_type, mapped_type>(k, mapped_type()));
+			return temp->key.second;
+		}
+		key_compare	key_comp() const
+		{
+			return key_compare(__cmp);
+		}
+		//? What the heck is this function doing. V
+		// value_compare	value_comp() const
+		// {
+			
+		// }
+		void erase (iterator first, iterator last)
+		{
+			while (first != last)
+			{
+				erase(first);
+				first++;
+			}
+		};
+		void		erase(iterator position)
+		{
+			//TODO: (ΦзΦ). . . fuck this.
+			if (__TreeRoot.searchNode(__TreeRoot.__root, (*position).first))
+			{
+				__TreeRoot.deleteNode((*position).first);
+				__Size--;
+			}
+		}
+		size_type	erase (const key_type& k)
+		{
+			int ret = 0;
+			if (__TreeRoot.searchNode(__TreeRoot.__root, k))
+				ret = 1;
+			__TreeRoot.deleteNode(k);
+			if (ret && __Size > 0)
+				__Size--;
+			return ret;
+		}
+
+		void	swap(map& x)
+		{
+			map	temp;
+
+			temp	= x;
+			x 		= *this;
+			x		= temp;
+		}
+
+		iterator	find(const key_type& k)
+		{
+			typename AvlBST<value_type, key_compare, Alloc>::Node*	temp;
+
+			temp = __TreeRoot.searchNode(__TreeRoot.__root, k);
+			if (temp)
+				return (iterator(AvlBST<value_type, key_compare, Alloc>(__TreeRoot.__root, temp)));
+			
+				return iterator(AvlBST<value_type, key_compare, Alloc>());
+		}
+		allocator_type get_allocator() const
+		{
+			return allocator_type();
+		};
 		//! Currently they're public for testing only.
 		//TODO: return it private after finishing tests.
 		public:
